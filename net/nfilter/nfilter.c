5c3cb5792701fb813f59ccd82c85c505e8468816
Webb Horn <webbhorn@mit.edu>
2013-12-04 16:34:37 -0600

    Block facebook pings dynamically.

--- net/nfilter/nfilter.c
+++ net/nfilter/nfilter.c
@@ -17,31 +17,7 @@ static struct _hashtable *policymap;
 #define FACEBOOK_ADDR 460258477
 static struct nf_hook_ops p;
 
-/*
- * a simple bsearch
- * TODO: export group_search from groups.c.
- */
-int groups_search(const struct group_info *group_info, kgid_t grp)
-{
-	unsigned int left, right;
-
-	if (!group_info)
-		return 0;
-
-	left = 0;
-	right = group_info->ngroups;
-	while (left < right) {
-		unsigned int mid = (left+right)/2;
-		if (gid_gt(grp, GROUP_AT(group_info, mid)))
-			left = mid + 1;
-		else if (gid_lt(grp, GROUP_AT(group_info, mid)))
-			right = mid;
-		else
-			return 1;
-	}
-	return 0;
-}
-
+int written;
 
 
 unsigned int hook_function(unsigned int hooknum,
@@ -50,6 +26,7 @@ unsigned int hook_function(unsigned int hooknum,
 			const struct net_device *out,
 			int (*okfn)(struct sk_buff *))
 {
+	int i;
 
 	struct iphdr * ip_header = (struct iphdr *) skb_network_header(skb);
 	__be32 daddr = ip_header->daddr;
@@ -58,10 +35,23 @@ unsigned int hook_function(unsigned int hooknum,
 	const struct cred *cc = current_cred();
 	struct group_info *netgroup_info = get_group_info(cc->netgroup_info);
 	struct user_namespace *user_ns = current_user_ns();
-	kgid_t nid = make_kgid(user_ns, (gid_t) 42);
-	if (groups_search(netgroup_info, nid))
-		printk(KERN_INFO "Caller is NID 42.\n");
-	put_group_info(netgroup_info);
+	for (i = 0; i < netgroup_info->ngroups; i++) {
+		struct _list *policylist;
+		kgid_t knid = GROUP_AT(netgroup_info, i);
+		gid_t nid = from_kgid_munged(user_ns, knid);
+		printk(KERN_INFO "nid: %d\t", (__u32)nid);
+		policylist = get(policymap, nid, daddr);
+		printk(KERN_INFO "policylist = %p\n", policylist);
+
+		if (policylist == NULL)
+			return NF_ACCEPT;
+
+		printk(KERN_INFO "blocked? %d\n", policylist->val->blocked);
+		if (policylist->val->blocked)
+			return NF_DROP;
+		else
+			return NF_ACCEPT;
+	}
 
 	/*
 	 * For each nid in calling process:
@@ -80,14 +70,17 @@ unsigned int hook_function(unsigned int hooknum,
 	 */
 
 	// Drop all facebook packets
-	if (daddr == FACEBOOK_ADDR)
-		return NF_DROP;
+	/*
+	 * if (daddr == FACEBOOK_ADDR)
+	 * 	return NF_DROP;
+	 */
 
 	return NF_ACCEPT;
 }
 
 static int nfilter_init(void)
 {
+	int retput;
 	printk(KERN_INFO "Loaded nfilter module\n");
 
 	p.hook = hook_function;
@@ -99,6 +92,10 @@ static int nfilter_init(void)
 
 	/* Initialize the policymap */
 	policymap = init_hash_table(HASH_TABLE_SIZE);
+
+	/* Block facebook for nid 42 */
+	retput = put(policymap, 42, FACEBOOK_ADDR, true);
+	printk(KERN_INFO "put returned %d\n", retput);
 
 	return 0;
 }
@@ -164,6 +161,7 @@ void free(struct _hashtable *hashtable) {
 			kfree(temp->key);
 			kfree(temp->val);
 			kfree(temp);
+			printk(KERN_INFO "Freed key, val, liststruct.\n");
 		}
 	}
 
@@ -179,8 +177,10 @@ __u32 hash(struct _nidkey *key, struct _hashtable *hashtable) {
 }
 
 int key_eq(struct _nidkey *a, struct _nidkey *b) {
-	return (((__u32)a->nid == (__u32)b->nid) && 
-	        ((__u32)a->ip_addr == (__u32)b->ip_addr));
+	int eq= (((__u32)a->nid == (__u32)b->nid) && 
+	         ((__u32)a->ip_addr == (__u32)b->ip_addr));
+	printk(KERN_INFO "equal keys? %d\n", eq);
+	return eq;
 }
 
 struct _list *get(struct _hashtable *hashtable, gid_t nid, __be32 ip_addr) {
@@ -191,17 +191,24 @@ struct _list *get(struct _hashtable *hashtable, gid_t nid, __be32 ip_addr) {
 	};
 
 	__u32 hashval = hash(&key, hashtable);
+	printk(KERN_INFO "get hashval: %d\n", hashval);
 	for (list = hashtable->table[hashval]; list != NULL; list = list->next) {
-		if (key_eq(&key, list->key))
+		printk(KERN_INFO "In for loop...\n");
+		if (key_eq(&key, list->key)) {
+			printk(KERN_INFO "Checking list...\n");
 			return list;
+		} else {
+			printk(KERN_INFO "Not equal...\n");
+		}
 	}
+	printk(KERN_INFO "Out of for loop...\n");
 
 	return NULL;
 }
 
-int put(struct _hashtable *table, gid_t nid, __be32 ip_addr, int blocked) {
+int put(struct _hashtable *hashtable, gid_t nid, __be32 ip_addr, int blocked) {
 	struct _nidkey *key;
-	struct _nidpoilcy *val;
+	struct _nidpolicy *val;
 	struct _list *new_list;
 	struct _list *current_list;
 	__u32 hashval;
@@ -223,11 +230,13 @@ int put(struct _hashtable *table, gid_t nid, __be32 ip_addr, int blocked) {
 	}
 	val->blocked = blocked;
 
-	hashval = hash(&key, hashtable);	
+	hashval = hash(key, hashtable);	
+	printk(KERN_INFO "put hashval: %d\n", hashval);
 	new_list = kmalloc(sizeof(struct _list), GFP_KERNEL);
 	if (!new_list) {
 		kfree(key);
 		kfree(val);
+		printk(KERN_INFO "Returning early...\n");
 		return -1;
 	}
 
@@ -236,12 +245,14 @@ int put(struct _hashtable *table, gid_t nid, __be32 ip_addr, int blocked) {
 		kfree(key);
 		kfree(val);
 		kfree(new_list);
+		printk(KERN_INFO "Returning early...\n");
 		return 2;  /* already exists */
 	}
 	new_list->key = key;
 	new_list->val = val;
 	new_list->next = hashtable->table[hashval];
 	hashtable->table[hashval] = new_list;
+	printk(KERN_INFO "hashtable->table[%d] = %p\n", hashval, hashtable->table[hashval]);
 
 	return 0;
 }
