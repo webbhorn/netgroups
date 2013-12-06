fba8e4b31b06e80d256f483478e37ebeab825fb6
Julian A Gonzalez <jugonz97@mit.edu>
2013-12-06 18:03:12 -0500

    integrated modules now live in net/nfilter/ and can be built with a single call to make. policy changes seem to be happening but do not affect users even with the correct nid and uid

--- net/nfilter/set_policy/set_policy.c
+++ net/nfilter/set_policy/set_policy.c
@@ -1,4 +1,4 @@
-/* Character driver test
+/* Set_Policy Kernel Module
  * Uses code from Linux Devices Drivers, 3rd edition &
  * http://pete.akeo.ie/2011/08/writing-linux-device-driver-for-kernels.html
  * Julian Gonzalez, 6.858 Final Project Fall 2013
@@ -11,51 +11,35 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/types.h>
-#include <linux/mutex.h>
-#include <linux/kfifo.h>
 #include <linux/cdev.h>
 #include <linux/string.h>
 #include <linux/uidgid.h>
 #include <uapi/linux/ip.h>
+#include <asm/uaccess.h>
 
-#define PROG_NAME "simpleCharDriver"
-#define MAX_MSGS 64
-#define MAX_IPs 64
+#include "../include/nfilter.h"
+#include "../include/ngpolicy.h"
+#include "set_policy.h"
 
-// global vars: sue me
-static dev_t device_nums;
-struct device* sysfs_device;
-static struct class *fifo_device_class;
-static struct cdev *fifo_cdev;
-static DEFINE_KFIFO(inputFIFO, char, MAX_MSGS);
-static DEFINE_MUTEX(kfifo_mutex);
-// from example: keep track of message lengths
-static ssize_t fifo_msg_lens[MAX_MSGS];
-// indices for message len array
-static int msg_len_rd, msg_len_write;
+// String constants
+const char * set_prefix = "set";
+const char * mode_blacklist = "b";
+const char * mode_whitelist = "w";	
 
 static ssize_t read_dummy(struct file *filp, char __user *buf,
 		size_t count, loff_t *f_pos) {
-	int return_code;
-	ssize_t bytes_copied;
+	char tocopy[] = "Function not implemented\n";
 	
-	if (kfifo_is_empty(&inputFIFO)) {
-		msg_len_rd = 0;
-		printk(KERN_INFO "FIFO was empty!");
-	} else if (msg_len_rd + 1 == msg_len_write) {
-		printk(KERN_INFO "read entire buffer");
-		msg_len_rd = 0;
-		return 0;
+	if (*f_pos > 0) {
+		return 0; // Don't return anything on subsequent reads
+	}
+	
+	// Tell user we don't do anything	
+	if (copy_to_user(buf, tocopy, sizeof(tocopy)) ) {
+		return -EFAULT;
 	}
 
-	// Send data to user space, keep return code, bytes copied and update pointer
-	// in message length array
-	return_code = kfifo_to_user(&inputFIFO, buf, fifo_msg_lens[msg_len_rd],
-		&bytes_copied);	
-	msg_len_rd = (msg_len_rd + 1);
-
-	// Return any error, or else the number of bytes pushed to userland
-	return return_code ? return_code : bytes_copied;
+	return count;
 }
 
 static int open_dummy(struct inode *inode, struct file *filp) {
@@ -65,32 +49,11 @@ static int open_dummy(struct inode *inode, struct file *filp) {
   		return -EACCES;
 	}
 
-	// ask mutex for lock
-	if (!mutex_trylock(&kfifo_mutex)) {
-		printk(KERN_INFO "Queue access is denied: already open in some other process\n");
-		return -EBUSY;
-	}
-
 	return 0;
 }
 
 static int release_dummy(struct inode *inode, struct file *filp) {
-	// release the queue's mutex
-	mutex_unlock(&kfifo_mutex);
-
 	return 0;
-}
-
-__be32 make_ipaddr(__u8 b1, __u8 b2, __u8 b3, __u8 b4) {
-        __be32 addr = 0;
-        addr |= b4;
-        addr = addr << 8;
-        addr |= b3;
-        addr = addr << 8;
-        addr |= b2;
-        addr = addr << 8;
-        addr |= b1;
-        return addr;
 }
 
 // This defines how the file in /dev is arranged
@@ -101,22 +64,17 @@ static struct file_operations f_ops = {
 };
 
 // Writing to this file adds a policy
-static ssize_t sysfile_add_to_kfifo(struct device* dev, struct device_attribute* attr,
+static ssize_t sysfile_set_policy(struct device* dev, struct device_attribute* attr,
 	const char* buf, size_t count) {	
-	// STRING CONSTANTS...refactor these into a .h file sometime soon
-	const char * set_prefix = "set";
-	const char * mode_blacklist = "b";
-	const char * mode_whitelist = "w";
-	
-	int policy_mode = 0;	/* 0 is unset, 1 is blacklist, 2 whitelist */	
 
+	ngmode_t policy_mode;	// Policy type: see ngpolicy.h
 	char *uid; // Pointer to UID string
 	char *nid; // Pointer to NID string
 	char *mode; // Pointer to policy mode string
 	uid_t uid_val; // UID as UID type
 	gid_t nid_val; // NID as GID type
-	
-	__be32 ip_addrs[MAX_IPs]; // Store IP addresses
+
+	__be32 ip_addrs[MAX_IPs]; // IP address array	
 	__u8 octals[4]; // Store IP octals
 
 	char *ip; // Pointer to IP address string
@@ -127,8 +85,13 @@ static ssize_t sysfile_add_to_kfifo(struct device* dev, struct device_attribute*
 	__u8 octal_val; // Octal as __u8 type
 	__be32 parsed_IP; // Parsed IP as IP type
 
+	struct _list* matching_policy; // policy that comes from hashtable lookup
+	struct _nidpolicy* inserted_policy; // actual inserted policy
+	
 	int ip_i; // IP address loop var
 	int octal_i; // Octal loop var
+	int ng_policy_i; // ng_policy loop var
+	int ng_policy_err; // Error code returned by put_ngpolicy
 
 	if ( strnicmp(buf, set_prefix, strlen(set_prefix)) != 0 ) { // check set prefix
 		printk("Invalid action specified. Valid actions are 'set' or 'get'.\n");
@@ -175,10 +138,10 @@ static ssize_t sysfile_add_to_kfifo(struct device* dev, struct device_attribute*
 
 	// parse policy mode: blacklist or whitelist
 	if (strnicmp(mode, mode_blacklist, strlen(mode_blacklist)) == 0) {
-		policy_mode = 1;
+		policy_mode = NG_BLACKLIST;
 		mode = mode + strlen(mode_blacklist) + 1;
 	} else if (strnicmp(mode, mode_whitelist, strlen(mode_whitelist)) == 0) {
-		policy_mode = 2;
+		policy_mode = NG_WHITELIST;
 		mode = mode + strlen(mode_whitelist) + 1;
 	} else {
 		printk(KERN_INFO "Invalid policy mode specified.\n");
@@ -189,7 +152,7 @@ static ssize_t sysfile_add_to_kfifo(struct device* dev, struct device_attribute*
 	// Parse IP addresses and save them	
 	// need octals to be unsigned ints (specifically __u8's)
 	ip = mode; // beginning of IP address list
-
+	
 	for (ip_i=0; ; ip_i++) {
 		end_of_ip = strchr(ip, ' '); // Split IP by space
 		if (end_of_ip) { // null terminate to allow strchr, etc. to work on intermediate strings
@@ -225,7 +188,7 @@ static ssize_t sysfile_add_to_kfifo(struct device* dev, struct device_attribute*
 	
 		// Make IP address type from octals	
 		parsed_IP = make_ipaddr(octals[0], octals[1], octals[2], octals[3]);
-		ip_addrs[ip_i] = parsed_IP;
+		ip_addrs[ip_i] = parsed_IP; // save in array
 		printk(KERN_INFO "Parsed IP is: %d %d %d %d\n", octals[0], octals[1], octals[2], octals[3]);		
 
 		ip = end_of_ip + 1; // Parse next IP...
@@ -234,6 +197,42 @@ static ssize_t sysfile_add_to_kfifo(struct device* dev, struct device_attribute*
 		}
 	}	
 
+
+	// Now, ip_addrs holds IP's...and ip_i holds the number of them
+	write_lock(&ngpolicymap_rwlk);	// Get write_lock to put new policy
+	ng_policy_err = put_ngpolicy(uid_val, nid_val, policy_mode);
+	if (ng_policy_err != 0) {
+		printk(KERN_INFO "put_ngpolicy returned: %d\n", ng_policy_err);
+		goto err;
+	}
+	// Now, get the policy just inserted
+	matching_policy = get_ngpolicy(uid_val, nid_val);
+	do {
+		if (!matching_policy) { // check for NULL policy
+			printk("Inserted policy could not be referenced!\n");
+			goto err;
+		}
+		
+		struct _nidkey *mp_key = matching_policy->key;	
+		if ((mp_key->uid == uid_val) && (mp_key->nid == nid_val)) {
+			inserted_policy = matching_policy->val; // found our policy
+			break;
+		} else {
+			matching_policy = matching_policy->next;
+		}
+	} while (true);
+
+	// Now, have policy. Add IP's to it
+	for (ng_policy_i = 0; ng_policy_i <= ip_i; ng_policy_i++) {
+		printk(KERN_INFO "Added ip to policy.\n");
+		if (add_ip_to_ngpolicy(inserted_policy, ip_addrs[ng_policy_i]) != 0) {
+			printk(KERN_INFO "Addding an IP to a policy failed.\n");
+		}
+	}
+
+	write_unlock(&ngpolicymap_rwlk); // Free read/write lock
+
+	printk(KERN_INFO "Policy has been set!\n");
 	goto ok;
 
 err:
@@ -245,25 +244,11 @@ end:
 	return count;
 }
 
-
-// Clear queue using sysfs
-static ssize_t sysfile_clear_kfifo(struct device* dev, struct device_attribute* attr,
-	const char* buf, size_t count) {
-	
-	// Just clear the queue, returning the count arg provided
-	kfifo_reset(&inputFIFO);
-	msg_len_rd = 0;
-	msg_len_write = 0;
-
-	return count;
-}
-
 // Declare the sysfs methods used
-static DEVICE_ATTR(fifo, S_IWUSR, NULL, sysfile_add_to_kfifo);
-static DEVICE_ATTR(reset, S_IWUSR, NULL, sysfile_clear_kfifo);
+static DEVICE_ATTR(set_policy, S_IWUSR, NULL, sysfile_set_policy);
 
 // Called on load of kernel module
-static int __init cd_tester_init(void) {
+static int __init policy_set_init(void) {
 	int return_val;
 	printk(KERN_INFO "%s has started to init\n", PROG_NAME);
 	
@@ -274,52 +259,44 @@ static int __init cd_tester_init(void) {
 	}
 
 	// Need to register the device in a class (virtual device here)	
-	if ((fifo_device_class = class_create(THIS_MODULE, "char_dev_class")) == NULL) {
+	if ((device_class = class_create(THIS_MODULE, CLASS_NAME)) == NULL) {
 		return_val = -1;
 		goto err_class_create;
 	}
 
 	// Now, create a device file (in /dev)
-	sysfs_device = device_create(fifo_device_class, NULL, device_nums, NULL, PROG_NAME);
+	sysfs_device = device_create(device_class, NULL, device_nums, NULL, PROG_NAME);
 	if (sysfs_device == NULL) {
 		return_val = -1;
 		goto err_dev_create;
 	}	
 
 	// Add files in /sys for adding and resetting the queue
-	return_val = device_create_file(sysfs_device, &dev_attr_fifo);
+	return_val = device_create_file(sysfs_device, &dev_attr_set_policy);
 	if (return_val < 0) {
-		printk(KERN_INFO "%s failed to create the fifo write file...\n", PROG_NAME);
-	}
-	return_val = device_create_file(sysfs_device, &dev_attr_reset);
-	if (return_val < 0) {
-		printk(KERN_INFO "%s failed to create the fifo reset file...\n", PROG_NAME);
+		printk(KERN_INFO "%s failed to create the set_policy sysfs file...\n", PROG_NAME);
 	}
 
 	// Need to register a character device
 	// Do this by allocing/initing a cdev struct
-	fifo_cdev = cdev_alloc();
-	cdev_init(fifo_cdev, &f_ops);
-	fifo_cdev->owner = THIS_MODULE;
+	policy_cdev = cdev_alloc();
+	cdev_init(policy_cdev, &f_ops);
+	policy_cdev->owner = THIS_MODULE;
 
 	// Add device to kernel
-	return_val = cdev_add(fifo_cdev, device_nums, 1);
+	return_val = cdev_add(policy_cdev, device_nums, 1);
 	if (return_val < 0) {
 		goto err_cdev_add;
 	}
 
 	// Finish setup
-	INIT_KFIFO(inputFIFO);
-	msg_len_rd = 0;
-	msg_len_write = 0;
 	return 0; // setup OK, else, destroy everything created
 
 err_cdev_add:
-	device_remove_file(sysfs_device, &dev_attr_reset);
-	device_remove_file(sysfs_device, &dev_attr_fifo);
-	device_destroy(fifo_device_class, device_nums);
+	device_remove_file(sysfs_device, &dev_attr_set_policy);
+	device_destroy(device_class, device_nums);
 err_dev_create:
-	class_destroy(fifo_device_class);
+	class_destroy(device_class);
 err_class_create:
 	unregister_chrdev_region(device_nums, 1);
 err:	
@@ -328,22 +305,19 @@ err:
 }
 
 // Called on unload of kernel module
-
-static void __exit cd_tester_cleanup(void) {
-	mutex_destroy(&kfifo_mutex);
-	cdev_del(fifo_cdev);
-	device_remove_file(sysfs_device, &dev_attr_reset);
-	device_remove_file(sysfs_device, &dev_attr_fifo);
-	device_destroy(fifo_device_class, device_nums);
-	class_destroy(fifo_device_class);
+static void __exit policy_set_cleanup(void) {
+	cdev_del(policy_cdev);
+	device_remove_file(sysfs_device, &dev_attr_set_policy);
+	device_destroy(device_class, device_nums);
+	class_destroy(device_class);
 	unregister_chrdev_region(device_nums, 1);
 	printk(KERN_INFO "%s has cleaned up and exited\n", PROG_NAME);
 }
 
-module_init(cd_tester_init);
-module_exit(cd_tester_cleanup);
+module_init(policy_set_init);
+module_exit(policy_set_cleanup);
 
 // Module info
 MODULE_AUTHOR("Julian Gonzalez");
-MODULE_DESCRIPTION("Simple FIFO character device");
+MODULE_DESCRIPTION("Policy setter for NID/nfilter module");
 MODULE_LICENSE("GPL");
