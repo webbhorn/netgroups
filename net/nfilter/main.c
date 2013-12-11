ee905ff5e0da87075ca8567127699f29a677c84e
Webb Horn <webbhorn@mit.edu>
2013-12-10 23:38:00 -0600

    comments and credit updates.

--- net/nfilter/main.c
+++ net/nfilter/main.c
@@ -8,6 +8,7 @@ extern int policy_set_init(void);
 extern void policy_set_cleanup(void);
 
 static int __init init(void) {
+	/* TODO: Check error codes */
 	int retcode;
 	retcode = nfilter_init();
 	retcode = policy_set_init();
@@ -23,5 +24,5 @@ module_init(init);
 module_exit(exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Tim Donegan <donegan@mit.edu>, Webb Horn <webbhorn@mit.edu>, Julian Gonzalez <>");
-MODULE_DESCRIPTION("858 proj fix later");
+MODULE_AUTHOR("Tim Donegan <donegan@mit.edu>, Webb Horn <webbhorn@mit.edu>, Julian Gonzalez <jugonz97@mit.edu>");
+MODULE_DESCRIPTION("6.858 netgroups project");
